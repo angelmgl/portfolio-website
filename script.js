be14962e095dc58adbe296a03d5e55fdd16bcdd5
Angel Mgl <angelemegeele@gmail.com>
2021-03-19 18:08:08 -0300

    Navigation created

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const $hamburgerMenu = document.querySelector("#navigation .nav-icon");
+const $navContent = document.querySelector(".nav-content");
+const $closeNavButton = document.querySelector(".nav-content .close-btn");
+const $navLinks = document.querySelectorAll(".nav-content nav ul li a");
 const $scrollButton = document.querySelector(".scroll-top");
 
 // scroll TOP Button Events
@@ -14,3 +18,20 @@ if ($scrollButton) {
         window.scrollTo(0, 0);
     });
 }
+
+// Hamburger Menu events
+$hamburgerMenu.addEventListener("click", () => {
+    $navContent.classList.add("show");
+    document.body.style.overflow = "hidden";
+});
+
+$closeNavButton.addEventListener("click", () => {
+    $navContent.classList.remove("show");
+    document.body.style.overflow = "initial";
+});
+
+$navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        $navContent.classList.remove("show");
+    });
+});
